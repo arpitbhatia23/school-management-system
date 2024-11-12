739248fb7a76bbae62880270d8acc416f52637a3
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-11-12 11:49:43 +0530

    added

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,6 +1,9 @@
 
 import mongoose, { Schema } from "mongoose";
-
+import studentProfile from "./studentprofile";
+import teacherProfile from "./teacherprofile";
+import argon2 from "argon2"
+import jwt from "jsonwebtoken"
 const userSchema= new mongoose.Schema({
 name : {
     type: String,
@@ -17,6 +20,7 @@ password:{
 },
 gender:{
     type:String,
+    enum:["male","female","transgender"],
     required:true
 },
 role:{
@@ -40,15 +44,47 @@ profile_image:{
 
 userSchema.pre("save", function (next) {
     if (this.role === "student") {
-      const { error } = studentProfileSchema.validateSync(this.profile);
+      const { error } = studentProfile.validateSync(this.profile);
       if (error) return next(new Error(`Invalid student profile: ${error.message}`));
     } else if (this.role === "teacher") {
-      const { error } = teacherProfileSchema.validateSync(this.profile);
+      const { error } = teacherProfile.validateSync(this.profile);
       if (error) return next(new Error(`Invalid teacher profile: ${error.message}`));
     }
     next();
   });
   
+
+userSchema.pre("save",async function (next) {
+    if(this.isModified("password")) return next()
+        this.password=await argon2.hash(this.password,{type:argon2.argon2id}) 
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await argon2.verify(this.password,password)
+}
+userSchema.methods.generateaccessToken=async function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+
+    },
+    process.env.ACCESSTOKENSECRET,{
+        expiresIn:process.env.ACCESSTOKENEXPIRY
+    }
+)
+}
+
+userSchema.methods.generaterefreshtoken=async function(){
+    return jwt.sign({
+        _id:this.id
+    },
+    process.env.REFRESHTOKENSECRET,
+    {
+        expiresIn:process.env.REFRESHTOKENEXPIRY
+    }
+)
+}
 
 
 export const User = mongoose.model("User",userSchema)
