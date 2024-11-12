3dcbbb98e2574395ba610be34ce90835d8854ccf
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-11-12 11:52:22 +0530

    added

--- backend/src/models/studentprofile.js
+++ backend/src/models/studentprofile.js
@@ -1,49 +1,58 @@
-import { Schema } from 'mongoose'
+import { Schema } from 'mongoose';
 const studentProfile = new Schema({
-    class :{
-        type:String,
-        required:true
-    },
-    DOB:{
-        type: String,
-        required:true
-    },
-    religion :{
-         type :String,
-         required:true
-    },
-    blood_group :{
-        type:String,
-        enum:['A_pos', 'A_neg', 'B_pos', 'B_neg', 'O_pos', 'O_neg', 'AB_pos', 'AB_neg'],
-        required:true
-    },
-    nationality :{
-        type:String,
-        required:true
-    },
-    parents_Detail :{
-        type:Schema.Types.ObjectId,
-    },
-    address :{
-        type:String,
-        required:true
-    },
-    category:{
-        type:String,
-        enum :['general','OBC','SC','ST'],
-        required:true
-    },
-    admission_Date:{
-          type:String,
-          required:true
-    },
-    attendence:{
-          type:String,
-          required:true
-    },
-    roll_no:{
-type:String,
-required:true
-    }
-})
-export default studentProfile
+  class: {
+    type: String,
+    required: true,
+  },
+  DOB: {
+    type: String,
+    required: true,
+  },
+  religion: {
+    type: String,
+    required: true,
+  },
+  blood_group: {
+    type: String,
+    enum: [
+      'A_pos',
+      'A_neg',
+      'B_pos',
+      'B_neg',
+      'O_pos',
+      'O_neg',
+      'AB_pos',
+      'AB_neg',
+    ],
+    required: true,
+  },
+  nationality: {
+    type: String,
+    required: true,
+  },
+  parents_Detail: {
+    type: Schema.Types.ObjectId,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ['general', 'OBC', 'SC', 'ST'],
+    required: true,
+  },
+  admission_Date: {
+    type: String,
+    required: true,
+  },
+  attendence: {
+    type: String,
+    required: true,
+  },
+  roll_no: {
+    type: String,
+    required: true,
+  },
+});
+export default studentProfile;
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,90 +1,93 @@
-
-import mongoose, { Schema } from "mongoose";
-import studentProfile from "./studentprofile";
-import teacherProfile from "./teacherprofile";
-import argon2 from "argon2"
-import jwt from "jsonwebtoken"
-const userSchema= new mongoose.Schema({
-name : {
-    type: String,
-    required: true},
-email :{
-    type:String}  ,
-phone_no:{
-    type:Number,
-    required:true
-}   ,
-password:{
-    type:String,
-    required:true
-},
-gender:{
-    type:String,
-    enum:["male","female","transgender"],
-    required:true
-},
-role:{
-    type:String,
-    enum:["student","admin","teacher"],
-},
-profile:{
-    type:Schema.Types.Mixed,
-    required:true
-},
-profile_image:{
-    type:String,
-    required:ture
-}
-
-
-},
-{
-    timestamps: true
-})
-
-userSchema.pre("save", function (next) {
-    if (this.role === "student") {
-      const { error } = studentProfile.validateSync(this.profile);
-      if (error) return next(new Error(`Invalid student profile: ${error.message}`));
-    } else if (this.role === "teacher") {
-      const { error } = teacherProfile.validateSync(this.profile);
-      if (error) return next(new Error(`Invalid teacher profile: ${error.message}`));
-    }
-    next();
-  });
-  
-
-userSchema.pre("save",async function (next) {
-    if(this.isModified("password")) return next()
-        this.password=await argon2.hash(this.password,{type:argon2.argon2id}) 
-    next()
-})
-
-userSchema.methods.isPasswordCorrect=async function(password){
-    return await argon2.verify(this.password,password)
-}
-userSchema.methods.generateaccessToken=async function(){
-    return jwt.sign({
-        _id:this._id,
-        email:this.email,
-
+import mongoose, { Schema } from 'mongoose';
+import studentProfile from './studentprofile';
+import teacherProfile from './teacherprofile';
+import argon2 from 'argon2';
+import jwt from 'jsonwebtoken';
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    process.env.ACCESSTOKENSECRET,{
-        expiresIn:process.env.ACCESSTOKENEXPIRY
-    }
-)
-}
+    email: {
+      type: String,
+    },
+    phone_no: {
+      type: Number,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female', 'transgender'],
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['student', 'admin', 'teacher'],
+    },
+    profile: {
+      type: Schema.Types.Mixed,
+      required: true,
+    },
+    profile_image: {
+      type: String,
+      required: ture,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-userSchema.methods.generaterefreshtoken=async function(){
-    return jwt.sign({
-        _id:this.id
+userSchema.pre('save', function (next) {
+  if (this.role === 'student') {
+    const { error } = studentProfile.validateSync(this.profile);
+    if (error)
+      return next(new Error(`Invalid student profile: ${error.message}`));
+  } else if (this.role === 'teacher') {
+    const { error } = teacherProfile.validateSync(this.profile);
+    if (error)
+      return next(new Error(`Invalid teacher profile: ${error.message}`));
+  }
+  next();
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) return next();
+  this.password = await argon2.hash(this.password, { type: argon2.argon2id });
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await argon2.verify(this.password, password);
+};
+userSchema.methods.generateaccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESSTOKENSECRET,
+    {
+      expiresIn: process.env.ACCESSTOKENEXPIRY,
+    },
+  );
+};
+
+userSchema.methods.generaterefreshtoken = async function () {
+  return jwt.sign(
+    {
+      _id: this.id,
     },
     process.env.REFRESHTOKENSECRET,
     {
-        expiresIn:process.env.REFRESHTOKENEXPIRY
-    }
-)
-}
+      expiresIn: process.env.REFRESHTOKENEXPIRY,
+    },
+  );
+};
 
-
-export const User = mongoose.model("User",userSchema)
+export const User = mongoose.model('User', userSchema);
