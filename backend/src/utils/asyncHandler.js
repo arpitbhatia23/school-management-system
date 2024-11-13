63db9541b8b0c5a36ac707764e1dbf8b6fb041d5
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-11-13 10:31:13 +0530

    added

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+
+export const asyncHandler = (fn) =>async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code||500).json({message:error.message})
+    }
+}
