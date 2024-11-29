73d17b8c86bf3c30942ff0214d2f5d4216b62368
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-11-29 11:46:35 +0530

    admin complete

--- backend/src/router/admin.routes.js
+++ backend/src/router/admin.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyAdmin, verifyJwt } from "../middleware/auth.middelware.js";
-import { addsuject, getAllParents, getallsubject, getAllTeacher, getStudent ,getStudentById, getTeacherById, promoteStudents,getParentsById,addNewExpense,getAllExpense, updateParentsById} from "../controllers/admin.controller.js";
+import { addsuject, getAllParents, getallsubject, getAllTeacher, getStudent ,getStudentById, getTeacherById, promoteStudents,getParentsById,addNewExpense,getAllExpense, updateParentsById, updateSubject} from "../controllers/admin.controller.js";
 const router =Router()
 router.route('/getstudent').get(verifyJwt,verifyAdmin,getStudent)
  router.route('/getstudentbyid/:student_id').get(verifyJwt,verifyAdmin,getStudentById)
@@ -13,6 +13,7 @@ router.route("/getallsubject").get(verifyJwt,verifyAdmin,getallsubject)
 router.route('/getparentsById').get(verifyJwt,getParentsById)
 router.route('/addNewExpense').post(verifyJwt,addNewExpense)
 router.route('/getAllExpense').get(verifyJwt,getAllExpense)
-router.route('/updateParentsById').get(verifyJwt,verifyAdmin,updateParentsById)
+router.route('/updateParentsById').patch(verifyJwt,verifyAdmin,updateParentsById)
+router.route('/updateSubject').patch(verifyJwt,verifyAdmin,updateSubject)
 export default router
 
