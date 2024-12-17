b41dbca4a19e7cc764c401e40ff6bf99a0bc0b19
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-12-17 11:29:13 +0530

    added

--- backend/src/router/student.routes.js
+++ backend/src/router/student.routes.js
@@ -1,9 +1,9 @@
 import Router from "express"
-import { verifyJwt } from "../middleware/auth.middelware.js"
+import { verifyJwt, verifystudent } from "../middleware/auth.middelware.js"
 import { genIdCard, getMonthlyAttendance, getResult,getexam } from "../controllers/students.controller.js"
 const router=Router()
-router.route("/genidcard").get(verifyJwt,genIdCard)
-router.route('/getMonthlyAttendance').get(verifyJwt,getMonthlyAttendance)
-router.route('/getResult').get(verifyJwt,getResult)
-router.route("/getexam").get(verifyJwt,getexam)
+router.route("/genidcard").get(verifyJwt,verifystudent, genIdCard)
+router.route('/getMonthlyAttendance').get(verifyJwt,verifystudent,getMonthlyAttendance)
+router.route('/getResult').get(verifyJwt,verifystudent,getResult)
+router.route("/getexam").get(verifyJwt,verifystudent,getexam)
 export default router
