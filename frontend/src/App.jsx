e88e060a436d5bd7b8179d80e80afc56c94d1e9a
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-12-25 10:59:14 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from '@/components/Navbar';
+import Login from '@/components/Login';
 
 function App() {
   return (
     <>
-      {/* <Appsidebar/> */}
       <Navbar />
-    </>
-  );
+      <Login/>
+      </>
+        );
 }
 
 export default App;
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import img from "../assets/12.svg"
+const Login = () => {
+  return (
+    <div>
+
+        <div className='bg-orange-500  text-4xl flex justify-center items-center h-40 w-20 '>
+        <img src={img} alt="" />
+        </div>
+      
+    </div>
+  )
+}
+
+export default Login
