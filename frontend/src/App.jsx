a8d452f5170a010db57a2161a35448f330433739
TAMANNA <tamannakarthol320@gmail.com>
2024-12-25 11:31:08 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from '@/components/Navbar';
 import Login from '@/components/Login';
-
+import input from '@/components/input';
 function App() {
   return (
     <>
       <Navbar />
       <Login/>
+      <input/>
       </>
         );
 }
---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Input as input } from "@/components/ui/input"
+
+const Input = () => {
+  return (
+    <div>
+      <input/>
+      hello
+    </div>
+  )
+}
+
+export default Input
